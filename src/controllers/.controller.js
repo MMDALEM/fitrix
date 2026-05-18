29f8968446c29e332a825d36936ef24936934049
mmd alem <mmdalam.work@gmail.com>
2026-05-18 22:45:41 +0330

    'fix'

--- src/controllers/.controller.js
+++ src/controllers/.controller.js
@@ -38,7 +38,7 @@ module.exports = class controller {
       .toString()
       .toLowerCase()
       .replace(/\s+/g, "-")
-      .replace(/[^\u0600-\u06FF\w\-]+/g, "") // Persian + English
+      .replace(/[^\u0600-\u06FF\w\-]+/g, "")
       .replace(/\-\-+/g, "-")
       .replace(/^-+/, "")
       .replace(/-+$/, "");
